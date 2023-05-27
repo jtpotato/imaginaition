c5b77a23a1a92fb3fe53fe577164687b628ae176
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-27 11:38:10 +1000

    reusable (barely) styling

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ function Home() {
     <>
       <div className="w-screen h-screen flex items-center justify-center text-center">
         <div>
-          <h1 className="text-9xl font-bold m-0 p-4" id="imaginaition-title">Imaginaition</h1>
+          <h1 className="text-9xl font-bold m-0 p-4 imaginaition-glow">Imaginaition</h1>
           <p className="text-2xl text-white">
-            The product suite <b>for you</b>.
+            The product suite <b className="imaginaition-glow">for you</b>.
           </p>
         </div>
       </div>

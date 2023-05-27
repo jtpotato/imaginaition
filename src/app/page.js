c8afb7c7acf94174c44197182487e02f48ef6b31
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-27 15:10:15 +1000

    gradients are breaking for some reason

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./hero.css"
+import "./hero.css";
 
 function Home() {
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center text-center">
-        <div>
-          <h1 className="text-9xl font-bold m-0 p-4 imaginaition-glow">Imaginaition</h1>
-          <p className="text-2xl text-white">
-            The product suite <b className="imaginaition-glow">for you</b>.
-          </p>
+      <div className="bg-gradient-to-r from-red-400 to-black">
+        <div className="w-screen h-screen flex items-center justify-center text-center">
+          <div>
+            <h1 className="text-9xl font-bold m-0 p-4 imaginaition-glow">
+              Imaginaition
+            </h1>
+            <p className="text-2xl text-white">
+              The product suite <b className="imaginaition-glow">for you</b>.
+            </p>
+          </div>
         </div>
       </div>
     </>
